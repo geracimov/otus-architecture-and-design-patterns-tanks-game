3d8bda90dce6922269518a20e35aca75ad3c20a9
geracimov <geracimov@gmail.com>
2022-01-17 23:37:58 +0300

    ExceptionHandler common functions
    * hashCode of command/exception instances test

--- src/test/kotlin/ru/geracimov/otus/architecture_design_patterns/tanks/exception/handler/ExceptionHandlerTest.kt
+++ src/test/kotlin/ru/geracimov/otus/architecture_design_patterns/tanks/exception/handler/ExceptionHandlerTest.kt
@@ -1,6 +1,8 @@
 package ru.geracimov.otus.architecture_design_patterns.tanks.exception.handler
 
 import org.junit.jupiter.api.Test
+import ru.geracimov.otus.architecture_design_patterns.tanks.UObjectImpl
+import ru.geracimov.otus.architecture_design_patterns.tanks.adapter.MovableAdapter
 import ru.geracimov.otus.architecture_design_patterns.tanks.command.Command
 import ru.geracimov.otus.architecture_design_patterns.tanks.command.MoveCommand
 import ru.geracimov.otus.architecture_design_patterns.tanks.command.RotateCommand
@@ -39,4 +41,19 @@ internal class ExceptionHandlerTest {
         val hashCode2 = exceptionHandler.hashCodeOf(RotateCommand::class, IllegalStateException::class)
         assertNotEquals(hashCode1, hashCode2)
     }
+
+    @Test
+    fun newInstancesCommandExceptionReturnSameResultTest() {
+        val command1 = MoveCommand(MovableAdapter(UObjectImpl(HashMap())))
+        val command2 = MoveCommand(MovableAdapter(UObjectImpl(HashMap())))
+        val exception1 = IllegalStateException()
+        val exception2 = IllegalStateException()
+        assertNotEquals(command1, command2)
+        assertNotEquals(exception1, exception2)
+
+        val hashCode1 = exceptionHandler.hashCodeOf(command1, exception1)
+        val hashCode2 = exceptionHandler.hashCodeOf(command2, exception2)
+        assertEquals(hashCode1, hashCode2)
+    }
+
 }
