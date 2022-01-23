f456e6cec7a087c81c8e83c1821fc0d14f4b915f
geracimov <geracimov@gmail.com>
2022-01-23 20:55:30 +0300

    MacroCommand
    * Реализовать простейшая макрокоманда и тесты к ней

---
+++ src/test/kotlin/ru/geracimov/otus/architecture_design_patterns/tanks/command/MacroCommandTest.kt
@@ -0,0 +1,35 @@
+package ru.geracimov.otus.architecture_design_patterns.tanks.command
+
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.mockito.kotlin.doThrow
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.times
+import org.mockito.kotlin.verify
+import ru.geracimov.otus.architecture_design_patterns.tanks.exception.CommandException
+import kotlin.test.assertFailsWith
+
+internal class MacroCommandTest {
+    private lateinit var someCommand1: Command
+    private lateinit var someCommand2: Command
+    private lateinit var someCommand3: Command
+
+
+    @BeforeEach
+    internal fun setUp() {
+        someCommand1 = mock {}
+        someCommand2 = mock {
+            on { execute() } doThrow CommandException("Command thrown some exception")
+        }
+        someCommand3 = mock {}
+    }
+
+    @Test
+    fun execute() {
+        val macroCommand = MacroCommand(listOf(someCommand1, someCommand2, someCommand3))
+        assertFailsWith<CommandException>(block = { macroCommand.execute() })
+        verify(someCommand1).execute()
+        verify(someCommand2).execute()
+        verify(someCommand3, times(0)).execute()
+    }
+}
